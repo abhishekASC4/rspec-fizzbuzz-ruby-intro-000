c0c1b9adf991d1a88b46d548334b96e62c21a6e4
Abhishek Ramjit <arox44@gmail.com>
2017-09-05 21:56:25 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-    if int % 5 == 0
-      "FizzBuzz"
-    end
   elsif int % 5 == 0
     "Buzz"
-  elsif int % 3 == 0 && int % 5 == 0
+  elsif ((int % 3 == 0) && (int % 5 == 0))
     "FizzBuzz"
   else
     nil
