81928e53ad2bd79a7e712dc5aa48fa07ab21cd4a
Abhishek Ramjit <arox44@gmail.com>
2017-09-05 21:39:03 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+def fizzbuzz(int)
+  if int % 3 == 0
+    "Fizz"
+    if int % 5 == 0
+      "FizzBuzz"
+  end
+  if int % 5 == 0
+    "Buzz"
+    if int % 3 == 0
+      "FizzBuzz"
+    end
+  end
+  nil
+end
